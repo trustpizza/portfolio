c8fa81f2021e93152272d5272dd878b43e3d3f68
Axel <maxelolsson@gmail.com>
2025-03-20 14:32:50 -0600

    fix rotation

--- src/components/Homepage/HomepageCarousel.jsx
+++ src/components/Homepage/HomepageCarousel.jsx
@@ -1,7 +1,7 @@
 import placeholder from '../../assets/placeholder.jpg'; // Adjust the path as needed
 
 const Gallery = ({ images = [] }) => {
-  const rotationPattern = [3, -3, 3, -3, 3]; // Rotation pattern
+  const rotationPattern = [3, -2, 3, -2, 3]; // Rotation pattern
   const minImages = 10;
 
   // Pad the images array with placeholders if it's shorter than minImages
@@ -17,12 +17,15 @@ const Gallery = ({ images = [] }) => {
           // Calculate the rotation value using the rotation pattern
           const rotationValue = rotationPattern[index % rotationPattern.length];
 
+          // Determine the class name based on the rotation value
+          const rotationClass = rotationValue < 0 ? `-rotate-${Math.abs(rotationValue)}` : `rotate-${rotationValue}`;
+          console.log(rotationClass)
           return (
             <div
               key={index}
               className={`m-4 carousel-item relative aspect-9/10 w-56 rounded-xl bg-base-200`}
             >
-              <img src={image} alt={`Gallery Image ${index + 1}`} className={`rounded-md object-cover w-full h-full rotate-${rotationValue}`} />
+              <img src={image} alt={`Gallery Image ${index + 1}`} className={`rounded-md object-cover w-full h-full ${rotationClass} `} />
             </div>
           );
         })}
