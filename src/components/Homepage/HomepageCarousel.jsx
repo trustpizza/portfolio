713a0530744fbd11e902105d0c77b720058152d5
Axel <maxelolsson@gmail.com>
2025-03-21 07:04:39 -0600

    Tweak carousel

--- src/components/Homepage/HomepageCarousel.jsx
+++ src/components/Homepage/HomepageCarousel.jsx
@@ -1,7 +1,7 @@
 import placeholder from '../../assets/placeholder.jpg'; // Adjust the path as needed
 
 const Gallery = ({ images = [] }) => {
-  const rotationPattern = [3, -2, 3, -2, 3]; // Rotation pattern
+  const rotationPattern = [5, -5, 5, -5]; // Rotation pattern
   const minImages = 10;
 
   // Pad the images array with placeholders if it's shorter than minImages
@@ -12,7 +12,7 @@ const Gallery = ({ images = [] }) => {
 
   return (
     <div className="mt-16 sm:mt-20 overflow-x-auto">
-      <div className="carousel carousel-end rounded-box max-w-screen-sm lg:max-w-screen-md">
+      <div className="carousel carousel-center rounded-box max-w-screen-sm lg:max-w-screen-md">
         {paddedImages.map((image, index) => {
           // Calculate the rotation value using the rotation pattern
           const rotationValue = rotationPattern[index % rotationPattern.length];
@@ -23,7 +23,7 @@ const Gallery = ({ images = [] }) => {
           return (
             <div
               key={index}
-              className={`m-4 carousel-item relative aspect-9/10 w-56 rounded-xl bg-base-200`}
+              className={`m-4 carousel-item w-72 relative aspect-9/10 sm:w-56 rounded-xl`}
             >
               <img src={image} alt={`Gallery Image ${index + 1}`} className={`rounded-md object-cover w-full h-full ${rotationClass} `} />
             </div>
